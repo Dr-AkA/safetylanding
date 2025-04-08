effb0e6f4b1d53ffb8fc723ae31ca61a82fdf6c9
Dr-AkA <akarmuhialdeen@gmail.com>
2025-04-08 22:41:46 +0200

    keywords added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,19 @@ const dmSans = DM_Sans({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Safety",
   description: "Safety group",
+  keywords: [
+    'gefahrstoffe', 'gefährdungsbeurteilungen gefahrstoffe', 'ehs software', 'betriebanweisung',
+    'ehs safety software', 'software ehs', 'ehs management systems', 'ehs management software',
+    'environmental health and safety management software', 'health and safety software',
+    'best ehs software', 'ehs compliance software', 'top 10 ehs software', 'health & safety software',
+    'iso19011', 'audit managerial', 'iso 27001 lead auditor', 'iso 27001 auditor',
+    'audit management', 'unterweisungen', 'maßnahmen', 'Gefahrdungsbeurteilungen',
+    'hse', 'hs&e', 'h&s executive', 'health safety & environment', 'hse solutions', 'hse 2',
+    'health and safety executive', 'hse health and safety', 'hse health safety and environment',
+    'hse uk', 'hse DE', 'hse Germany', 'hse Deutschland', 'hse planning'
+  ]
 };
+
 
 export default function RootLayout({
   children,
