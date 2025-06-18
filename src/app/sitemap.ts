7fe3c065130643ea5472dbf5b928e69716e7f30d
akar ali <akar@akars-MacBook-Air.local>
2025-06-18 09:17:59 +0200

    robots and sitemaps

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,7 +4,22 @@ import { MetadataRoute } from "next";
 export default async function sitemap():Promise<MetadataRoute.Sitemap>{
     return [
         {
-            url:''
+            url:'https://new.safety-doors.com',
+            lastModified:new Date(),
+            changeFrequency:'monthly',
+            priority:1,
+        },
+        {
+            url:'https://new.safety-doors.com/about',
+            lastModified:new Date(),
+            changeFrequency:'monthly',
+            priority:1
+        },
+        {
+            url:'https://new.safety-doors.com',
+            lastModified: new Date(),
+            changeFrequency:'monthly',
+            priority:1
         }
     ]
 }
